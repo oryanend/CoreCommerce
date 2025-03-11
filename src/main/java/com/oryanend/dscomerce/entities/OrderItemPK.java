359f35e4bcb5cc0f36bf31a558369aefda073493
oryanend <ryanyamim@gmail.com>
2025-03-11 19:39:23 -0300

    Order Item

---
+++ src/main/java/com/oryanend/dscomerce/entities/OrderItemPK.java
@@ -0,0 +1,38 @@
+package com.oryanend.dscomerce.entities;
+
+import jakarta.persistence.*;
+
+@Embeddable
+public class OrderItemPK {
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public OrderItemPK() {
+    }
+
+    public OrderItemPK(Order order, Product product) {
+        this.order = order;
+        this.product = product;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
