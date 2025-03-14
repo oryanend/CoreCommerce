d9b6e3fa73d273bf362729b9e8d01cc08694ad72
oryanend <ryanyamim@gmail.com>
2025-03-14 13:52:51 -0300

    ProductController - findById

--- src/main/java/com/oryanend/dscomerce/controllers/ProductController.java
+++ src/main/java/com/oryanend/dscomerce/controllers/ProductController.java
@@ -1,9 +1,12 @@
 package com.oryanend.dscomerce.controllers;
 
+import com.oryanend.dscomerce.dto.ProductDTO;
 import com.oryanend.dscomerce.entities.Product;
 import com.oryanend.dscomerce.repositories.ProductRepository;
+import com.oryanend.dscomerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,13 +17,11 @@ import java.util.Optional;
 public class ProductController {
 
     @Autowired
-    private ProductRepository productRepository;
+    private ProductService productService;
 
-    @GetMapping
-    public String teste(){
-        Optional<Product> result = productRepository.findById(1L);
-        Product product = result.get();
-        return product.getName();
+    @GetMapping(value = "/{id}")
+    public ProductDTO teste(@PathVariable Long id){
+        return productService.findById(id);
     }
 
 }
