f06b7897ae0306985c96206a7d767500a07e8d9e
oryanend <ryanyamim@gmail.com>
2025-03-14 13:03:28 -0300

    Hashcode and Equals Added

--- src/main/java/com/oryanend/dscomerce/entities/OrderItemPK.java
+++ src/main/java/com/oryanend/dscomerce/entities/OrderItemPK.java
@@ -2,6 +2,8 @@ package com.oryanend.dscomerce.entities;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
 @Embeddable
 public class OrderItemPK {
     @ManyToOne
@@ -34,5 +36,18 @@ public class OrderItemPK {
 
     public void setProduct(Product product) {
         this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItemPK that = (OrderItemPK) o;
+        return Objects.equals(order, that.order) && Objects.equals(product, that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(order, product);
     }
 }
