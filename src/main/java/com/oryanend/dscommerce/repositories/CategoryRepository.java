4e27f5c815d6a22a17b5b2223576706b394e83fa
oryanend <ryanyamim@gmail.com>
2025-03-29 21:57:51 -0300

    Get Categories

---
+++ src/main/java/com/oryanend/dscommerce/repositories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.oryanend.dscommerce.repositories;
+
+import com.oryanend.dscommerce.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
