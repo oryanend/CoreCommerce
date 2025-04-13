e204feed0d934b72d0dd5c491b9d0ee25b7dcddd
oryanend <ryanyamim@gmail.com>
2025-04-13 01:43:59 -0300

    Get All Categories - Swagger

--- src/main/java/com/oryanend/dscommerce/controllers/CategoryController.java
+++ src/main/java/com/oryanend/dscommerce/controllers/CategoryController.java
@@ -2,6 +2,11 @@ package com.oryanend.dscommerce.controllers;
 
 import com.oryanend.dscommerce.dto.CategoryDTO;
 import com.oryanend.dscommerce.services.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +23,15 @@ public class CategoryController {
     private CategoryService service;
 
     @GetMapping
+    @Operation(summary = "Buscar todas as categorias",
+            description = "Retorna todos as categorias desse sistema.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Produto encontrado com sucesso",
+                    content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "404", description = "Produto não encontrado", content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor",
+                    content = @Content(schema = @Schema(hidden = true)))
+    })
     public ResponseEntity<List<CategoryDTO>> findAll(){
         List<CategoryDTO> list = service.findAll();
         return ResponseEntity.ok().body(list);
